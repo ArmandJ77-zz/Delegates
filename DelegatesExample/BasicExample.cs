8f4bad82ed7f8947724b8bdf80ba9b490516dc2a
armand <armand@iplatform.co.za>
2017-08-24 14:41:56 +0200

    update

--- DelegatesExample/BasicExample.cs
+++ DelegatesExample/BasicExample.cs
@@ -4,24 +4,21 @@ namespace DelegatesExample
 {
     class BasicExample
     {
+        //Declare a delegate which retruns an int and accepts a double as parameter
+        delegate int RoundUpDelegate(double input);
+
         static void Main(string[] args)
         {
-            //Declare a delegate which retruns an int and accepts a double as parameter
-            delegate int RoundUpDelegate(double input);
+            //instantiate a new delegate type and point it to your method
+            RoundUpDelegate calc = RoundUp;
 
-            static void Main(string[] args)
-            {
-                //instantiate a new delegate type and point it to your method
-                RoundUpDelegate calc = RoundUp;
+            //use the delegate instance to invoke the RoundUp method
+            Console.WriteLine(calc(22.7));
+        }
 
-                //use the delegate instance to invoke the RoundUp method
-                Console.WriteLine(calc(22.7));
-            }
-
-            public static int RoundUp(double input)
-            {
-                return (int)Math.Ceiling(input);
-            }
+        public static int RoundUp(double input)
+        {
+            return (int)Math.Ceiling(input);
         }
     }
 }
