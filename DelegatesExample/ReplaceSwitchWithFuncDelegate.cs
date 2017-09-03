bff259118f315300dee03dfbd3ccab25512a221a
Armand Jordaan <armandjordaan@live.co.za>
2017-09-03 13:35:21 +0200

    Completed Example

--- DelegatesExample/ReplaceSwitchWithFuncDelegate.cs
+++ DelegatesExample/ReplaceSwitchWithFuncDelegate.cs
@@ -3,37 +3,7 @@ using System.Collections.Generic;
 
 namespace DelegatesExample
 {
-    //This is a basic example of replacing a switch statement
-    //granted that most of the business logic can be replaced 
-    //with lambdas 
-    class ReplaceSwitchWithFuncDelegate
-    {
-        static void Main(string[] args)
-        {
-            //Seed InputCollection
-            List<Product> ProductsCollection =
-                new List<Product> { new Product { Name = "Blade", Type = ProductType.Ultrabook } };
-
-            var SetMarketingInfo = new Dictionary<string, Func<string>>()
-            {
-                { ProductType.Ultrabook.ToString(), () => UltrabookInfo() },
-                { ProductType.Keyboard.ToString(), () => KeyboardInfo() },
-                { ProductType.Headsets.ToString(), () => HeadsetInfo() }
-            };
-
-
-            ProductsCollection.ForEach(x => );
-
-        }
-
-        //These can also be converted to properties but in 
-        //a real world implementation this would each have 
-        //business logic
-        public static string UltrabookInfo() => "Ultra thin gaming laptop";
-        public static string KeyboardInfo() => "Has color changing LEDs";
-        public static string HeadsetInfo() => "Really comfy headsets";
-    }
-
+    //1 - Define object
     class Product
     {
         public string Name { get; set; }
@@ -41,19 +11,51 @@ namespace DelegatesExample
         public string MarketingInfo { get; set; }
     }
 
+    //2 - Define enums
     enum ProductType
     {
         Ultrabook,
         Keyboard,
-        Headsets        
+        Headsets
     }
 
-    //Some Basic functions to simulate external class Logic calls
-    class Calculations
+    class ReplaceSwitchWithFuncDelegate
     {
-        public Add(int input, int incrementer)
+        //This is a basic example of replacing a switch statement
+        //granted that most of the business logic can be replaced 
+        //with lambdas 
+        static void Main(string[] args)
         {
+            //3 - Seed InputCollection
+            List<Product> ProductsCollection =
+                new List<Product> {
+                    new Product { Name = "Razer Blade Pro", Type = ProductType.Ultrabook },
+                    new Product { Name = "Razer Orinata", Type = ProductType.Keyboard },
+                    new Product { Name = "Kraken Pro V2", Type = ProductType.Headsets },
+                };
 
+            //4 - Define a dictionary which accepts a string as a condition and retruns a delagte result of type string
+            //The replacement of the traditional switch, although its important to note that if each
+            //func returns a diffirent type then use a return func<dynamic> instead of func<string>
+            var SetMarketingInfo = new Dictionary<string, Func<string>>()
+            {
+                { ProductType.Ultrabook.ToString(), () => UltrabookInfo() },
+                { ProductType.Keyboard.ToString(), () => KeyboardInfo() },
+                { ProductType.Headsets.ToString(), () => HeadsetInfo() }
+            };
+
+            // 6 - Iterate over the collection and assign the correct marketing information
+            ProductsCollection.ForEach(x => x.MarketingInfo = SetMarketingInfo[x.Type.ToString()].Invoke());
+
+            // 7 - Iterate over colection and display the products
+            ProductsCollection.ForEach(x => Console.WriteLine(x.MarketingInfo));
         }
+
+        // 5 - These can also be converted to properties but in a real
+        // world implementation this would each contain some business logic
+        // or reads from a repository
+        public static string UltrabookInfo() => "Ultra thin gaming laptop";
+        public static string KeyboardInfo() => "Has color changing LEDs";
+        public static string HeadsetInfo() => "Really comfy headsets";
     }
 }
