eb639fef9ff55088120e4f021e54c4f75e56b22f
Armand Jordaan <armandjordaan@live.co.za>
2017-09-04 21:07:00 +0200

    Added action delegate

---
+++ DelegatesExample/BasicAction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DelegatesExample
+{
+    class BasicAction
+    {
+        static void Main(string[] args)
+        {
+            Action<double> printActionDelegate = i => Console.WriteLine(Math.Ceiling(i).ToString());
+
+            printActionDelegate(10);
+        }
+    }
+}
